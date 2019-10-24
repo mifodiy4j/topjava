50abc0c5a75a671c8e38beecba81aaa696e6c83f
MikhailovSV <MikhailovSV@sibintek.ru>
2019-10-24 09:43:26 +0300

    HW3_2: create table MEALS

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -9,3 +10,7 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, description, calories) VALUES
+  (100000, 'обед', 1000),
+  (100001, 'завтрак', 510);
