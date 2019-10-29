a63044ef2a69c9eedede7aa12122fd7a126f1b22
MikhailovSV <MikhailovSV@sibintek.ru>
2019-10-29 16:55:44 +0300

    HW3_3: refactoring JdbcMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -1,18 +1,66 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public class JdbcMealRepository implements MealRepository {
 
+    private static final RowMapper<Meal> ROW_MAPPER = new RowMapper<Meal>() {
+        @Override
+        public Meal mapRow(ResultSet rs, int rowNumber) throws SQLException {
+            return new Meal(rs.getInt("id"), rs.getTimestamp("dateTime").toLocalDateTime(),
+                    rs.getString("description"), rs.getInt("calories"));
+        }
+    };
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private final SimpleJdbcInsert insertMeal;
+
+    @Autowired
+    public JdbcMealRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.insertMeal = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("meals")
+                .usingGeneratedKeyColumns("id");
+
+        this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        MapSqlParameterSource map = new MapSqlParameterSource()
+                .addValue("id", meal.getId())
+                .addValue("user_id", userId)
+                .addValue("dateTime", Timestamp.valueOf(meal.getDateTime()))
+                .addValue("description", meal.getDescription())
+                .addValue("calories", meal.getCalories());
+        if (meal.isNew()) {
+            Number newId = insertMeal.executeAndReturnKey(map);
+            meal.setId(newId.intValue());
+        } else if (namedParameterJdbcTemplate.update("UPDATE meals SET " +
+                "description=:description, calories=:calories, " +
+                "dateTime=:dateTime WHERE id=:id AND user_id=:user_id", map)
+                == 0) {
+            return null;
+        }
+        return meal;
     }
 
     @Override
--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -27,10 +27,11 @@ CREATE TABLE user_roles
 
 CREATE TABLE meals
 (
+  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
   user_id          INTEGER                 NOT NULL,
-  date_time        TIMESTAMP DEFAULT now() NOT NULL,
+  dateTime        TIMESTAMP DEFAULT now() NOT NULL,
   description      VARCHAR                 NOT NULL,
   calories         INTEGER                 NOT NULL,
-  CONSTRAINT meals_idx UNIQUE (user_id, date_time),
+  CONSTRAINT meals_idx UNIQUE (user_id, dateTime),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
